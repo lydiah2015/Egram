8ac306446f8302857eafa896d1e27fdd3956789a
Elle <mitchellemakini15@gmail.com>
2019-05-17 10:52:01 +0300

    created Profile model

--- elle/models.py
+++ elle/models.py
@@ -2,14 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Image:
-    image=models.ImageField
+    image=models.ImageField(upload_to = 'today-photos.html/')
     image_name=models.CharField(max_length=15)
     image_caption=models.CharField(max_length=30)
     comments=models.CharField(max_length=50)
-    likes=models
+    # likes=models
+    profile_photo=models.ForeignKey(Profile)
 
+    
 class Profile:
-    profile_photo=models.ImageField
+    profile_photo=models.ImageField(upload_to = 'today-photos.html/')
     biography=models.CharField(max_length=30)
 
 
