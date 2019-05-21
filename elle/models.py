a8155df6e4591a3eb739a28a8dc359d0da93aaab
Elle <mitchellemakini15@gmail.com>
2019-05-21 14:10:31 +0300

    added models

--- elle/models.py
+++ elle/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 
 # Create your models here.
-class Image:
-    image=models.ImageField(upload_to = 'today-photos.html/')
-    image_name=models.CharField(max_length=15)
-    image_caption=models.CharField(max_length=30)
-    comments=models.CharField(max_length=50)
-    # likes=models
-    profile_photo=models.ForeignKey(Profile)
 
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'gramm/')
+    biography = models.CharField(max_length= 50)
+
+
+class Images(models.Model):
+    image = models.ImageField(upload_to = 'gramm/')
+    image_name = models.CharField(max_length=15)
+    image_caption = models.CharField(max_length=30)
+    # comments = models.CharField(max_length=50)
+    # likes=models.IntegerField()
+    profile_photo=models.ForeignKey(Profile)
+   
+
+class Comments(models.Model):
+    profile_photo=models.ForeignKey(Profile)
     
-class Profile:
-    profile_photo=models.ImageField(upload_to = 'today-photos.html/')
-    biography=models.CharField(max_length=30)
 
 
