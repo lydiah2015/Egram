4cf47ebc8776055e888fc26dbeb515eb8f89c305
Elle <mitchellemakini15@gmail.com>
2019-05-21 14:10:53 +0300

    added urls

--- elle/urls.py
+++ elle/urls.py
@@ -1,6 +1,11 @@
-from django.conf.urls import url,include
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+     url('^$',views.today_photos,name='today_photos'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
