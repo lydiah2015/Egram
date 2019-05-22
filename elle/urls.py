44c72da8e4779ebbb10192becd1db4c194302cbe
Elle <mitchellemakini15@gmail.com>
2019-05-22 17:25:42 +0300

    urls

--- elle/urls.py
+++ elle/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns=[
      url('^$',views.today_photos,name='today_photos'),
+     url(r"^add/image/$", views.add_image, name = "add_image"),
+     url(r"^accounts/profile/$", views.my_profile, name = "my_profile"),
 ]
 
 if settings.DEBUG:
